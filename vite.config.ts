095f7063d8f9a6ec88ceece782ef351648bedd5d
Éric D. <15271+edas@users.noreply.github.com>
2026-01-25 19:57:01 +0100

    Update TypeScript configuration and Vite setup to support path aliases for improved module resolution. Add baseUrl and paths in tsconfig.json, and configure aliasing in vite.config.ts for components, lib, and styles directories.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 import yaml from '@modyfi/vite-plugin-yaml'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [yaml(), react(), viteSingleFile()],
+  resolve: {
+    alias: {
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/lib': path.resolve(__dirname, './src/lib'),
+      '@/styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
   build: {
     target: 'esnext',
     assetsInlineLimit: 100000000,
